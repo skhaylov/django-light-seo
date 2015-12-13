fa197103964480c30c918bc8189551a51ca1b98a
Sergey Khaylov <Sergey.Haylov@gmail.com>
2015-12-13 18:12:02 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8;
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -11,8 +11,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-light-seo',
-    version='0.1',
-    packages=['light_seo'],
+    version='0.2',
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='A simple Django app for SEO.',
