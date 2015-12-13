60f1145087a38531c05906b984ea37d82256c794
Sergey Khaylov <Sergey.Haylov@gmail.com>
2015-12-13 15:16:21 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+# coding=utf-8;
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-light-seo',
+    version='0.1',
+    packages=['light_seo'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app for SEO.',
+    long_description=README,
+    url='https://www.github.com/skhaylov/django-simple-seo',
+    author='Your Name',
+    author_email='yourname@example.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
